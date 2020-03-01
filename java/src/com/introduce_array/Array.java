26051cff5b4539ec79b98721ea1e7d8c15a0aa05
yunuswidodo <younews372@gmail.com>
2020-03-01 23:03:07 +0700

    introduce method

---
+++ java/src/com/introduce_array/Array.java
@@ -0,0 +1,21 @@
+package com.introduce_array;
+
+public class Array {
+    public static void main(String[] args) {
+        int [] numbers  = new int[4];
+        numbers[0] = 10;
+        numbers[1] = 15;
+        numbers[2] = 20;
+        numbers[3] = 25;
+
+        for (int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+
+        System.out.println("----------------------------------------");
+         int [] numbers2 = {10, 20, 30, 40};
+        for (int x = 0; x < numbers2.length; x ++){
+            System.out.println(numbers2[x]);
+        }
+    }
+}
